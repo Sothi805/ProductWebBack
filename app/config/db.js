dd888ea6f7e794e8af52786ccb54c23d8877a0f8
MONY-007 <you@example.com>
2024-12-11 16:05:26 +0700

    fixing

--- app/config/db.js
+++ app/config/db.js
@@ -1,10 +1,11 @@
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URL).then((res) => {
   console.log(res);
 
 }).catch((er) => {
-  console.log('PLOKC IS:', er);
+  console.log('error:', er);
 
 })
 module.exports = {
