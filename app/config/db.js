40729a56c8e44f8aac8a84e8738507597e7dd4cc
MONY-007 <you@example.com>
2024-12-11 15:50:38 +0700

    fixing

--- app/config/db.js
+++ app/config/db.js
@@ -1,15 +1,6 @@
-// require('dotenv').config();
-// module.exports = {
-//   database: {
-//     dialect: "mysql",
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USER,
-//     database: process.env.DB_NAME,
-//   },
-//   jwtSecret: process.env.JWT_SECRET
-// }
+
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://admin:admin%40123@cluster0.66tar.mongodb.net/survey_db?retryWrites=true&w=majority&appName=Cluster0").then((res) => {
+mongoose.connect(process.env.MONGODB_URL).then((res) => {
   console.log(res);
 
 }).catch((er) => {
