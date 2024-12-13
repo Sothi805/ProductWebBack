773bc00836a70a1f0a274abb40693838fd730c8b
MONY-007 <you@example.com>
2024-12-13 09:44:50 +0700

    fixing

--- app/models/form/form.js
+++ app/models/form/form.js
@@ -2,52 +2,58 @@
 const mongoose = require("mongoose");
 
 // Schema for Forms
-const formSchema = new mongoose.Schema({
-  product_name: {
-    type: String,
-    required: true,
+const formSchema = new mongoose.Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_type: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pre_Order", "In_Stock", "Out_of_Stock"],
+      default: "In_Stock",
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    label: {
+      type: String,
+    },
+    home_page_position: {
+      type: Number,
+    },
+    title: {
+      type: String,
+      required: false,
+    },
+    sub_title: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    total_price: {
+      type: Number,
+      required: false,
+    },
+    discount: {
+      type: String,
+      default: "0",
+    },
+    total_before_discount: {
+      type: Number,
+    },
   },
-  product_type: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["Pre_Order", "In_Stock","Out_of_Stock"],
-    default: "In_Stock",
-  },
-  image: {
-    type: String,
-    required: false,
-  },
-  label: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: false,
-  },
-  sub_title: {
-    type: String,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  total_price: {
-    type: Number,
-    required: false,
-  },
-  discount: {
-    type: String,
-    default: "0%",
-  },
-  total_before_discount: {
-    type: Number,
-  },
-}, {
-  timestamps: true, // Automatically adds createdAt and updatedAt fields
-});
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
 
 const Form = mongoose.model("Form", formSchema);
 
